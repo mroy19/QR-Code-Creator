5f962e42dfce432dd6454810e9eca4773b58d550
mroy19 <mroy19@students.kennesaw.edu>
2024-11-03 15:13:46 -0500

    Made edits to QR code creator and downloaded a qr code generator.

--- qrCreator/qr_creator.py
+++ qrCreator/qr_creator.py
@@ -4,13 +4,13 @@ sys.stdout.write("Press q and Enter for quit.\n")
 choice = ["y", "n", "q"]
 while True:
 	try:
-		qr_addr = input("Address: ")
+		qr_addr = input("Medication: ")
 		my_qr = qrcode.make(qr_addr)
 
 		if(qr_addr == choice[-1]):
 			sys.exit()
 
-		qr_save = input("Choose a name: ")
+		qr_save = input("Patient Name: ")
 		my_qr.save(qr_save)
 
 		my_qr.show()
@@ -33,41 +33,3 @@ while True:
 		sys.exit()
 	except KeyboardInterrupt:
 		sys.exit()
-"""
-##### OLD CODE - SEP 8 2021 ####
-import qrcode
-import webbrowser
-
-print("Press q and Enter for quit.\n")
-choice = ["y", "n"]
-while True:
-	try:
-		qr_addr = input("Address:")
-		my_qr = qrcode.make(qr_addr)
-
-		if(qr_addr == "q"):
-			break
-
-		qr_save = input("Choose a name:")
-		my_qr.save(qr_save)
-
-		my_qr.show()
-
-		#For Kali Linux users
-		while True:
-			choose = input("Do you want to check your address of QR code? (y / n) ")
-			if(choose == choice[0]):
-				webbrowser.open(qr_addr)
-				break
-			elif(choose == choice[1]):
-				break
-			else:
-				print("Error: Invalid input.\n")
-		#For Kali Linux users
-
-	except SyntaxError:
-		print("Error: If you are a Linux user, you should use the python3.\n")
-		break
-	except KeyboardInterrupt:
-		break
-"""
